37b27cf921367d461b8d10a034bca41e06ade995
Ryan Duchene <duchenerc@protonmail.com>
2021-09-10 21:18:08 -0500

    Initial commit

---
+++ sokoban/sokoban.cpp
@@ -0,0 +1,23 @@
+﻿// sokoban.cpp : Defines the entry point for the application.
+//
+#include "sokoban.hpp"
+
+int main(int argc, char* argv[])
+{
+   std::string filename;
+   if (argc < 2)
+   {
+      std::cout << "usage: sokoban <filename>" << std::endl;
+      return 1;
+   }
+   else
+   {
+      filename = argv[1];
+   }
+
+   std::ifstream fin{ filename };
+
+   fin.close();
+
+   return 0;
+}
---
+++ sokoban/sokoban.hpp
@@ -0,0 +1,10 @@
+﻿// sokoban.hpp : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+// TODO: Reference additional headers your program requires here.
